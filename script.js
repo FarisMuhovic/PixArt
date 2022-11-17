9e6ae94c7e8de390fee9b17d73e2a6562834efcf
FarisMuhovic <faris.fare123456@gmail.com>
2022-11-17 23:22:56 +0100

    responsivness

--- script.js
+++ script.js
@@ -1,9 +1,11 @@
 const canvasElement = document.getElementById("drawing-board");
 const canvas = canvasElement.getContext("2d");
-
-const pixelSize = 32;
+const pixelSize = 16;
 const size = 15;
-
+const drawingContainer = document.getElementById("fakeCanvas");
+for (let i = size ** 2; i > 0; i--) {
+  drawingContainer.insertAdjacentHTML("beforeend", `<div></div>`);
+}
 canvasElement.width = size * pixelSize;
 canvasElement.height = size * pixelSize;
 
@@ -13,3 +15,4 @@ canvasElement.addEventListener("mousedown", e => {
   // canvas.fillStyle = SELECTED COLOR!
   canvas.fillRect(CoordinateX, CoordinateY, pixelSize, pixelSize);
 });
+// try to make it full width and height
